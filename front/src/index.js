ec02e58dd920c164b7e91f167dbb107a8f12a10d
Sgt-Rodrigo <rod_raven@hotmail.com>
2024-02-25 18:47:19 -0300

    adds a 'back to the start' btn in home page

--- front/src/index.js
+++ front/src/index.js
@@ -7,3 +7,27 @@ import "./index-modules/formRenderer";
 import "./views/add_movie_form/add_movie_form";
 
 MovieLoader.loadAndRenderGrid();
+
+
+//*? return to top button
+
+const returnButton = document.querySelector('.return-btn');
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 100) { // Adjust 100 to the scroll position you prefer
+//         returnButton.style.display = 'block';
+//     } else {
+//         returnButton.style.display = 'none';
+//     }
+// });
+
+window.addEventListener('scroll', () => {
+    const section = document.querySelector('#grid-title');
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < window.innerHeight / 2) {
+        returnButton.style.display = 'block';
+    } else {
+        returnButton.style.display = 'none';
+    }
+});
